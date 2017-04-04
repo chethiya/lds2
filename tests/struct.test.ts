50439582689aaf39e3fc1288ef89f5cd2b748a29
Chethiya Abeysinghe <chethiya@gmail.com>
2017-04-04 12:40:51 +0530

    struct tests

--- tests/struct.test.ts
+++ tests/struct.test.ts
@@ -1,24 +1,70 @@
 // First argument to 'describe' (which is defined by Jasmine) is the testing module that will
 // appear in test reports. The second argument is a callback containing the individual tests.
 
-import {Struct, Interfaces} from './../src/struct/struct';
+import { Struct, Interfaces } from './../src/struct/struct';
 import * as Types from './../src/types';
 
 describe("Struct", function () {
- it("arguments", function () {
-  let Person = Struct([
-   { name: 'name', type: Types.Int32, length: 3 },
-   { name: 'age', type: Types.Uint8 },
-   { name: 'height', type: Types.Float64 }
-  ]);
+ let Person = Struct([
+  { name: 'name', type: Types.Int32, length: 3 },
+  { name: 'age', type: Types.Uint8 },
+  { name: 'height', type: Types.Float64 }
+ ]);
+ it("init and assign", function () {
   let p: Interfaces.Struct = new Person({
    name: [1, 2, 3],
    age: 29,
    height: 160
   });
-  console.log(p.get().toString());
-  expect((p.get() as Interfaces.Value).age).toEqual(29);
+
+  expect(p.get()).toEqual({
+   name: [1, 2, 3], age: 29, height: 160
+  });
+  expect(p.name).toEqual([1, 2, 3]);
+  p.name = [4, 5, 6];
+  expect(p.get()).toEqual({
+   name: [4, 5, 6], age: 29, height: 160
+  });
+  expect(p.name).toEqual([4, 5, 6]);
+  p.age = 31;
+  expect(p.age).toEqual(31);
+  expect(p.get()).toEqual({
+   name: [4, 5, 6], age: 31, height: 160
+  });
+  p.set(100, Person._NAME, 2);
+  expect(p.get(Person._NAME, 2)).toEqual(100);
+  expect(p.get()).toEqual({
+   name: [4, 5, 100], age: 31, height: 160
+  });
+  p.set([9, 9], Person._NAME);
+  expect(p.get()).toEqual({
+   name: [9, 9, 100], age: 31, height: 160
+  });
  });
+
+ it("copyFrom", function() {
+  let p: Interfaces.Struct = new Person({
+   name: [1, 2, 3],
+   age: 29,
+   height: 160
+  });
+  let p2: Interfaces.Struct = new Person();
+
+  expect((p2.get() as any).toJSON()).toEqual({
+   name: [0, 0, 0], age: 0, height: 0
+  });
+
+  p2.copyFrom(p);
+  expect((p2.get() as any).toJSON()).toEqual({
+   name: [1, 2, 3], age: 29, height: 160
+  });
+  p.height = 23.3;
+  expect(p.height).toEqual(23.3);
+  expect((p.get() as any).height).toEqual(23.3);
+
+  expect(p2.height).toEqual(160);
+  expect((p2.get() as any).height).toEqual(160);
+ })
 });
 
 /*
